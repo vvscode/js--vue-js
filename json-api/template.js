e210ec1dbae6848d1bb1c58ad6322b830b614db1
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-10-24 12:04:08 +0400

    Add template

---
+++ json-api/template.js
@@ -0,0 +1,23 @@
+[
+  {
+    'repeat(11)': {
+      id: '{{index()}}',
+      isActive: '{{bool()}}',
+      balance: '{{floating(1000, 4000, 2, "$0,0.00")}}',
+      picture: 'http://placehold.it/128x128',
+      age: '{{integer(20, 40)}}',
+      accessLevel: '{{random("guest", "user", "admin")}}',
+      firstName: '{{firstName()}}',
+      lastName: '{{surname()}}',
+      company: '{{company().toUpperCase()}}',
+      email: function (tags) {
+        // Email tag is deprecated, because now you can produce an email as simple as this:
+        return (this.firstName + '.' + this.lastName + '@' + this.company + tags.domainZone()).toLowerCase();
+      },
+      phone: '+7 {{phone()}}',
+      address: '{{integer(100, 999)}} {{street()}}, {{city()}}, {{state()}}, {{integer(100, 10000)}}',
+      about: '{{lorem(1, "paragraphs")}}',
+      registered: '{{moment(this.date(new Date(2014, 0, 1), new Date())).format("DD.MM.YYYY")}}'
+    }
+  }
+]
